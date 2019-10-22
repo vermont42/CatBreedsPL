acebdc7dc0ddc1962620628c7828b67d1c660c6d
vermont42 <vermontcoder@gmail.com>
2019-10-22 08:13:45 -0700

    convert to Swift 5/Xcode 11/iOS 13

--- CatBreeds/Views/BreedCell.swift
+++ CatBreeds/Views/BreedCell.swift
@@ -30,7 +30,7 @@ class BreedCell: UITableViewCell {
     fatalError("init(coder:) has not been implemented.")
   }
 
-  override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
     super.init(style: style, reuseIdentifier: reuseIdentifier)
     backgroundColor = Colors.blackish
     addSubview(photo)
